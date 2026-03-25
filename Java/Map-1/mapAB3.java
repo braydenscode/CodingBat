e3b34b55cf24f430f94cf9ad3128fa1ff5940f5e
Owner <braydens.coding@gmail.com>
2026-03-25 14:21:31 -0500

    Java/Map-1

---
+++ Java/Map-1/mapAB3.java
@@ -0,0 +1,12 @@
+public Map<String, String> mapAB3(Map<String, String> map) {
+    if(map.containsKey("a")) {
+        if(!map.containsKey("b")) {
+            map.put("b", map.get("a"));
+        }
+    } else if (map.containsKey("b")) {
+        if(!map.containsKey("a")) {
+            map.put("a", map.get("b"));
+        }
+    }
+    return map;
+}
